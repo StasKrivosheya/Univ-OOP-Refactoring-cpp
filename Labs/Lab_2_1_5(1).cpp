dc548daa9ad138d05dba0d96c11520fcda140273
StasKrivosheya <58636032+StasKrivosheya@users.noreply.github.com>
2019-12-08 17:31:51 +0200

    Update Lab_2_1_5(1).cpp

--- Labs/Lab_2_1_5(1).cpp
+++ Labs/Lab_2_1_5(1).cpp
@@ -1,37 +1,35 @@
-// Lab_2_1_5(1).cpp: определяет точку входа для консольного приложения.
-//
-
-#include "stdafx.h"
 #include <iostream>
-using namespace std;
+#include <string>
 
+std::string yearInfo(int year)
+{
+	bool isLeap = ((year % 4 == 0) && (year % 100 != 0)) || year % 400 == 0;
+	std::string info;
 
-void year_(int year) {
-	
-	if (((year % 4 == 0) and (year % 100 != 0)) or (year % 400 == 0)) {
-		cout << "Leap year\n";
-	}
-	else {
-		cout << "Common year\n";
-	}
+	if (isLeap)
+		info = "Leap year\n";
+	else
+		info = "Common year\n";
+
+	return info;
 }
 
-int main(int year) {
-		
-	cout << "2000 == Expected value == Leap year => Function Value == " ;
-	year_(2000);
-	cout << "2015 == Expected value == Common year => Function Value == ";
-	year_(2015);
-	cout << "1999 == Expected value == Common year => Function Value == ";
-	year_(1999);
-	cout << "1996 == Expected value == Leap year => Function Value == ";
-	year_(1996);
-	cout << "1900 == Expected value == Common year => Function Value == ";
-	year_(1900);
-	
+void printYearInfo(int year)
+{
+	std::cout << year << " == Expected value == Leap year => Function Value == "
+						<< yearInfo(year);
+}
+
+int main(int year)
+{
+
+	printYearInfo(2000);
+	printYearInfo(2015);
+	printYearInfo(1999);
+	printYearInfo(1996);
+	printYearInfo(1900);
+
 	system("pause");
 
 	return 0;
 }
-
-
